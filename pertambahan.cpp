c1c73b1296a13fe57edaed8a5f69590b1522d781
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-11-27 09:25:21 +0700

    pertambahan dalam cpp

---
+++ pertambahan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int bilangan1,bilangan2,tambah;
+ 
+ cin>>bilangan1;
+ cin>>bilangan2;
+ //memasukan bilangan yang ingin ditambah
+ 
+ tambah=bilangan1+bilangan2;
+ //pertambahan dilakukan
+ 
+ cout<<"Hasil pertambahan = "<<tambah<<endl;
+ //memunculkan hasil
+}
